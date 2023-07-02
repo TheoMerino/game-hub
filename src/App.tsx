885d1d3b6fd4ff3e883437fc022a6cbee44d4b70
Théo MERINO <theo.merino89@gmail.com>
2023-07-02 11:15:43 +0200

    build a navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,27 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
-import { Button } from "@chakra-ui/react";
+import { Button, Grid, GridItem, Show } from "@chakra-ui/react";
+import NavBar from "./components/NavBar";
 
 function App() {
-  return <Button colorScheme="blue">Toto</Button>;
+  return (
+    <Grid
+      templateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
+    >
+      <GridItem area={"nav"}>
+        <NavBar />
+      </GridItem>
+      <GridItem area={"main"} bgColor={"twitter.500"}>
+        Main
+      </GridItem>
+      <Show above="lg">
+        <GridItem area={"aside"} bgColor={"whatsapp.700"}>
+          Aside
+        </GridItem>
+      </Show>
+    </Grid>
+  );
 }
 
 export default App;
